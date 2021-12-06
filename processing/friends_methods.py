9fdd8cd9bb6d4b3d1750f25514f1596d75ea1372
DavidDaimond <strokatov91@mail.ru>
2021-12-06 22:29:16 +0300

    End to refactor Mutual FriendsList (mutual)

--- processing/friends_methods.py
+++ processing/friends_methods.py
@@ -30,36 +30,10 @@ class MutualOHE:
             return np.sum((data == self.checktable).astype(int), 0)
 
     def transform(self, fls):
-        if not (self.checktable is None):
-            t = np.zeros((self.checktable.shape[0],))
-            for x in fls:
-                t = np.vstack((t, self.transform_one(x)))
-            return t
+        if self.checktable is None:
+            return None
+        t = np.zeros((self.checktable.shape[0],))
+        for x in fls:
+            t = np.vstack((t, self.transform_one(x)))
+        return t
 
-
-def mutuals_clustering_pca(md, clusterizator, return_clusters=True, **kwargs):
-    m_ohe = MutualOHE()
-    persons_list = np.array(list(md.keys()))
-
-    m_ohe.fit(persons_list)
-    transformed_data = m_ohe.transform(list(md.values()))
-
-    pca = PCA(n_components=2)
-    points = pca.fit_transform(transformed_data)
-
-    # ax = plt.axes()
-    # ax.set(facecolor="black")
-
-    clusterizator.fit_predict(points)
-
-    for i in range(len(set(clusterizator.labels_))):
-        plt.scatter(points[:, 0][clusterizator.labels_ == i], points[:, 1][clusterizator.labels_ == i], 7)
-
-    if return_clusters:
-        clusters = []
-        for i in range(len(set(clusterizator.labels_))):
-            clusters.append(
-                list(
-                    zip(persons_list[clusterizator.labels_[1:] == i], [clusterizator.labels_ == i])
-                )
-            )
